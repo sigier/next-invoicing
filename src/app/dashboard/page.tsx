a423cc20c5870ba41378c74e2fae595cc62adcd8
sigier <jotulw@proton.me>
2025-01-28 12:17:50 +0200

    feat.: add new invoice form added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,26 +8,53 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { CirclePlus } from "lucide-react";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center items-center h-screen gap-6 text-center max-w-5xl mx-auto">
-      <h1 className="text-5xl font-bold mb-4">Dashboard</h1>
+    <main className="flex flex-col justify-center items-center h-screen gap-6 text-center max-w-5xl mx-auto my-12">
+      <div className="flex justify-between w-full">
+        <h1 className="text-3xl font-bold mb-4">Bills</h1>
+        <p>
+          <Button className="inline-flex gap-2" variant="ghost" asChild>
+            <Link href="invoice/new">
+              <CirclePlus className="h-4 w-4" />
+              Bills
+            </Link>
+          </Button>
+        </p>
+      </div>
       <Table>
         <TableCaption>A list of your recent invoices.</TableCaption>
         <TableHeader>
           <TableRow>
-            <TableHead className="w-[100px]">Invoice</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Method</TableHead>
-            <TableHead className="text-right">Amount</TableHead>
+            <TableHead className="w-[100px] p-4">Date</TableHead>
+            <TableHead className="p-4">Customer</TableHead>
+            <TableHead className="p-4">Email</TableHead>
+            <TableHead className="text-center p-4">Status</TableHead>
+            <TableHead className="text-right p-4">Value</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           <TableRow>
-            <TableCell className="font-medium">INV001</TableCell>
-            <TableCell>Paid</TableCell>
-            <TableCell>Credit Card</TableCell>
-            <TableCell className="text-right">$250.00</TableCell>
+            <TableCell className="font-medium text-left p-4">
+              <span className="font-semibold">10/15/2024</span>
+            </TableCell>
+            <TableCell className="text-left p-4">
+              <span className="font-semibold">Rogan S.</span>
+            </TableCell>
+            <TableCell className="text-left p-4">
+              <span>rogans@scuise.com</span>
+            </TableCell>
+            <TableCell className="text-center p-4">
+              <Badge className="rounded-full">Paid</Badge>
+            </TableCell>
+            <TableCell className="text-right p-4">
+              <span>$250.00</span>
+            </TableCell>
           </TableRow>
         </TableBody>
       </Table>
---
+++ src/app/invoice/new/page.tsx
@@ -0,0 +1,51 @@
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col justify-left h-screen gap-6 max-w-5xl mx-auto">
+      <div className="flex justify-between w-full">
+        <h1 className="text-3xl font-bold mb-4 mt-4">Create invoice</h1>
+      </div>
+      <form className="grid gap-4 max-w-xs">
+        <div>
+          <div className="mb-3">
+            <Label htmlFor="name" className="block font-semibold text-sm mb-1">
+              Billing name
+            </Label>
+            <Input id="name" name="name" type="text"></Input>
+          </div>
+
+          <div className="mb-3">
+            <Label htmlFor="email" className="block font-semibold text-sm mb-1">
+              Billing Email
+            </Label>
+            <Input id="email" name="email" type="email"></Input>
+          </div>
+
+          <div className="mb-3">
+            <Label htmlFor="value" className="block font-semibold text-sm mb-1">
+              Value
+            </Label>
+            <Input id="value" name="value" type="text"></Input>
+          </div>
+
+          <div className="mb-3">
+            <Label
+              htmlFor="description"
+              className="block font-semibold text-sm mb-1"
+            >
+              Description
+            </Label>
+            <Textarea id="description" name="description"></Textarea>
+          </div>
+          <div className="w-full font-semibold">
+            <Button>Submit</Button>
+          </div>
+        </div>
+      </form>
+    </main>
+  );
+}
