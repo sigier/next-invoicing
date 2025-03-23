40c15249191f0552f2ca0c894fa9c86697b829a3
sigier <jotulw@proton.me>
2025-03-23 16:18:03 +0200

    feat(auth): add passkey authentication support

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,6 +3,8 @@
 import * as Clerk from "@clerk/elements/common";
 import * as SignIn from "@clerk/elements/sign-in";
 import { Button } from "@/components/ui/button";
+import { AnimatePresence, motion } from "framer-motion";
+import Link from "next/link";
 
 export default function SignInPage() {
   return (
@@ -50,10 +52,17 @@ export default function SignInPage() {
             <p className="mb-4 text-center text-sm/5 text-neutral-500">
               Alternatively, sign in with these platforms
             </p>
-            <div className="space-y-2">
+            <div className="space-y-6">
+              <p className="text-center">
+                <SignIn.Passkey asChild>
+                  <Button className="w-full" variant="outline">
+                    Continue with Passkey
+                  </Button>
+                </SignIn.Passkey>
+              </p>
               <Clerk.Connection name="google" asChild>
                 <Button
-                  className="flex gap-2 w-full font-semibold"
+                  className="flex gap-2 w-full font-bold"
                   variant="outline"
                 >
                   <svg
@@ -63,6 +72,7 @@ export default function SignInPage() {
                     aria-hidden
                     className="size-4"
                   >
+                    <title>Google Logo</title>
                     <g clipPath="url(#a)">
                       <path
                         fill="currentColor"
@@ -82,12 +92,12 @@ export default function SignInPage() {
           </div>
           <p className="text-center text-sm text-neutral-500">
             Don&apos;t have an account?{" "}
-            <Clerk.Link
-              navigate="sign-up"
+            <Link
+              href={String(process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL)}
               className="rounded px-1 py-0.5 text-neutral-700 outline-none hover:bg-neutral-100 focus-visible:bg-neutral-100"
             >
               Sign up
-            </Clerk.Link>
+            </Link>
           </p>
         </SignIn.Step>
         <SignIn.Step
@@ -102,6 +112,7 @@ export default function SignInPage() {
                 viewBox="0 0 40 40"
                 className="mx-auto size-10"
               >
+                <title>Logo</title>
                 <mask
                   id="a"
                   width="40"
@@ -147,6 +158,7 @@ export default function SignInPage() {
                 viewBox="0 0 40 40"
                 className="mx-auto size-10"
               >
+                <title>Logo</title>
                 <mask
                   id="a"
                   width="40"
@@ -184,14 +196,77 @@ export default function SignInPage() {
               Login
             </SignIn.Action>
           </SignIn.Strategy>
+          <SignIn.Strategy name="totp">
+            <header className="text-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                className="mx-auto size-10"
+                viewBox="0 0 24 24"
+              >
+                <title>Logo</title>
+                <path d="M16 20V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v16" />
+                <rect width="20" height="14" x="2" y="6" rx="2" />
+              </svg>
+              <h1 className="mt-4 text-xl font-medium tracking-tight text-neutral-950">
+                Verify Authenticator Code
+              </h1>
+            </header>
+            <Clerk.GlobalError className="block text-sm text-red-600" />
+            <Clerk.Field name="code">
+              <Clerk.Label className="sr-only">Authenticator Code</Clerk.Label>
+              <Clerk.Input
+                type="otp"
+                required
+                className="flex justify-center gap-1"
+                render={({ value, status }) => (
+                  <div
+                    data-status={status}
+                    className="relative h-9 w-8 rounded-md bg-white ring-1 ring-inset ring-zinc-300 data-[status=selected]:bg-sky-400/10 data-[status=selected]:shadow-[0_0_8px_2px_theme(colors.sky.400/30%)] data-[status=selected]:ring-sky-400"
+                  >
+                    <AnimatePresence>
+                      {value && (
+                        <motion.span
+                          initial={{ opacity: 0, scale: 0.75 }}
+                          animate={{ opacity: 1, scale: 1 }}
+                          exit={{ opacity: 0, scale: 0.75 }}
+                          className="absolute inset-0 flex items-center justify-center text-zinc-950"
+                        >
+                          {value}
+                        </motion.span>
+                      )}
+                      {value}
+                    </AnimatePresence>
+                    {status === "cursor" && (
+                      <motion.div
+                        layoutId="otp-input-focus"
+                        transition={{ ease: [0.2, 0.4, 0, 1], duration: 0.2 }}
+                        className="absolute inset-0 z-10 rounded-[inherit] border border-sky-400 bg-sky-400/10 shadow-[0_0_8px_2px_theme(colors.sky.400/30%)]"
+                      />
+                    )}
+                  </div>
+                )}
+              />
+              <Clerk.FieldError className="mt-2 block text-xs text-red-600" />
+            </Clerk.Field>
+            <SignIn.Action submit asChild>
+              <Button className="w-full font-bold">Log In</Button>
+            </SignIn.Action>
+          </SignIn.Strategy>
           <p className="text-center text-sm text-neutral-500">
             Don&apos;t have an account?{" "}
-            <Clerk.Link
-              navigate="sign-up"
+            <a
+              href="/sign-up"
               className="rounded px-1 py-0.5 text-neutral-700 outline-none hover:bg-neutral-100 focus-visible:bg-neutral-100"
             >
               Sign up
-            </Clerk.Link>
+            </a>
           </p>
         </SignIn.Step>
       </SignIn.Root>
