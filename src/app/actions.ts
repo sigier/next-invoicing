9741f2437e6129b0a896c357ff61e7bcf8267394
sigier <jotulw@proton.me>
2025-03-24 10:55:13 +0200

    fix(auth): handle null userId error in auth response

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -3,10 +3,16 @@
 import { db } from "@/db";
 import { Invoices } from "@/db/schema";
 import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
 
 export async function createAction(formData: FormData) {
+  const { userId } = await auth();
   const value = Math.floor(parseFloat(String(formData.get("value")))) * 100;
   const description = formData.get("description") as string;
+
+  if (!userId) {
+    return;
+  }
 
   const result = await db
     .insert(Invoices)
